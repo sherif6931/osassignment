caf7d16a4e52ddc6c1862440f39a6e51ba8d6c5f
sherif6931 <sherif6931@github.com>
2026-01-02 13:20:59 -0600

    finished tasks per spec sheet

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "yelp.h" 
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init main_init(void) {
+    
+    show_ancestry();
+
+    show_task(); 
+    
+    return 0;
+}
+
+static void __exit main_exit(void) {
+    pr_info("Yelp is done.\n");
+}
+
+module_init(main_init);
+module_exit(main_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("YELP");
+MODULE_AUTHOR("ME");
